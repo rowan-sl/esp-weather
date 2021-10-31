e914f96a2e670cc60c657dfa0b1f8ae91247cd8a
Rowan S-L <rowan.with.cats@gmail.com>
2021-10-31 14:52:27 -0400

    added user config template

---
+++ include/secret_stuff_template.h
@@ -0,0 +1,6 @@
+// a template for secret_stuff.h, a file for putting things that should NOT be commited to github, make shure that it is never commited.
+// this is a template file for that, so when you are ready to run this, copy this to a file called secret_stuff.h, and enter your information.
+// make shure you never commit secret_stuff.h to github.
+
+const char* ssid = "Your network name here";
+const char* password = "Your network password here";
